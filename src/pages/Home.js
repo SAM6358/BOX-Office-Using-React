cd9170984251c0d7f9e615e67067dbba9eb4c980
Samarth Singh <samarthsingh6358@gmail.com>
2022-09-27 00:39:44 +0530

    added layout and created title

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import MainPageLayout from '../components/MainPageLayout';
 
 const Home = () => {
-  return <div>Welcome to Home Page.</div>;
+  return <MainPageLayout>Welcome to Home Page.</MainPageLayout>;
 };
 
 export default Home;
--- src/pages/Starred.js
+++ src/pages/Starred.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import MainPageLayout from '../components/MainPageLayout';
 
 const Starred = () => {
-  return <div>This is a starred page.</div>;
+  return <MainPageLayout>This is a starred page.</MainPageLayout>;
 };
 
 export default Starred;
