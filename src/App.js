06189c3a3ee217e8dac28559cdc8dd9da4f335d4
Samarth Singh <samarthsingh6358@gmail.com>
2022-09-26 23:51:30 +0530

    added react-router-dom for reedering web pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        This is Home page.
+      </Route>
+
+      <Route exact path="/starred">
+        This is starred page.
+      </Route>
+
+      <Route>Error! 404 Page not found.</Route>
+    </Switch>
+  );
 }
 
 export default App;
--- src/service-worker.js
+++ src/service-worker.js
@@ -1,5 +1,4 @@
-eslint - disable;
-/* eslint-disable no-restricted-globals */
+/* eslint-disable */
 
 // This service worker can be customized!
 // See https://developers.google.com/web/tools/workbox/modules
