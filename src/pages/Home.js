fca614550358ee57c3f91c9a1799099e96a1ecf3
Samarth Singh <samarthsingh6358@gmail.com>
2022-09-27 01:09:07 +0530

    added search bar and added fetch to fetch data from TV Maze API

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
 
 const Home = () => {
-  return <MainPageLayout>Welcome to Home Page.</MainPageLayout>;
+  const [input, setInput] = useState('');
+  const onSearch = () => {
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then(res => res.json())
+      .then(result => {
+        console.log(result);
+      });
+  };
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      onSearch();
+    }
+  };
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+    </MainPageLayout>
+  );
 };
 
 export default Home;
