b729160fd10bd50361c26f7edef74e095a14e23f
Samarth Singh <samarthsingh6358@gmail.com>
2022-09-28 00:11:36 +0530

    created cards for shows and people from API data

---
+++ src/components/show/ShowCard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const ShowCard = ({ id, name, image, summary }) => {
+  const summaryasText = summary
+    ? `${summary.split(' ').slice(0, 10).join(' ').replace(/<.+?>/g, '')}...`
+    : 'No Description';
+
+  return (
+    <div>
+      <div>
+        <img src={image} alt="show" />
+      </div>
+      <h1>{name}</h1>
+      <p>{summaryasText}</p>
+      <div>
+        <Link to={`/show/${id}`}> Read more... </Link>
+        <button type="button">Star me</button>
+      </div>
+    </div>
+  );
+};
+
+export default ShowCard;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { APIGet } from '../misc/Configure';
 import MainPageLayout from '../components/MainPageLayout';
+import ShowGrid from '../components/show/ShowGrid';
+import ActorGrid from '../components/actor/ActorGrid';
 
 const Home = () => {
   const [input, setInput] = useState('');
@@ -28,11 +30,11 @@ const Home = () => {
       return <div>No results Found.</div>;
     }
     if (results && results.length > 0) {
-      return results[0].show
-        ? results.map(item => <div key={item.show.id}>{item.show.name}</div>)
-        : results.map(item => (
-            <div key={item.person.id}>{item.person.name}</div>
-          ));
+      return results[0].show ? (
+        <ShowGrid data={results} />
+      ) : (
+        <ActorGrid data={results} />
+      );
     }
     return null;
   };
