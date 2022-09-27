63aa49522831c86f958eaf7f64444d2062a120f9
Samarth Singh <samarthsingh6358@gmail.com>
2022-09-27 20:35:26 +0530

    added search option for shows and actors

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,8 +5,12 @@ import MainPageLayout from '../components/MainPageLayout';
 const Home = () => {
   const [input, setInput] = useState('');
   const [results, setResults] = useState(null);
+  const [searchOptions, setsearchOptions] = useState('shows');
+
+  const isSearchShows = searchOptions === 'shows';
+
   const onSearch = () => {
-    APIGet(`/search/shows?q=${input}`).then(result => {
+    APIGet(`/search/${searchOptions}?q=${input}`).then(result => {
       setResults(result);
     });
   };
@@ -24,25 +28,50 @@ const Home = () => {
       return <div>No results Found.</div>;
     }
     if (results && results.length > 0) {
-      return (
-        <div>
-          {results.map(item => (
-            <div key={item.show.id}>{item.show.name}</div>
-          ))}
-        </div>
-      );
+      return results[0].show
+        ? results.map(item => <div key={item.show.id}>{item.show.name}</div>)
+        : results.map(item => (
+            <div key={item.person.id}>{item.person.name}</div>
+          ));
     }
     return null;
+  };
+
+  const onRadioChange = ev => {
+    setsearchOptions(ev.target.value);
   };
 
   return (
     <MainPageLayout>
       <input
         type="text"
+        placeholder="Search"
         onChange={onInputChange}
         onKeyDown={onKeyDown}
         value={input}
       />
+      <div>
+        <label htmlFor="search-shows">
+          Shows
+          <input
+            id="search-shows"
+            type="radio"
+            value="shows"
+            checked={isSearchShows}
+            onChange={onRadioChange}
+          />
+        </label>
+        <label htmlFor="search-actors">
+          Actors
+          <input
+            id="search-shows"
+            type="radio"
+            value="people"
+            checked={!isSearchShows}
+            onChange={onRadioChange}
+          />
+        </label>
+      </div>
       <button type="button" onClick={onSearch}>
         Search
       </button>
